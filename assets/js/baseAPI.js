e1330668076281b03b4a8924de5f47140284d92c
lizi <lizi@qq.com>
2021-01-18 21:47:26 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,21 @@
+var url = 'http://api-breakingnews-web.itheima.net';
 $.ajaxPrefilter(function(options) {
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    // 统一根路径
+    options.url = url + options.url;
+    // 统一身份认证
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 拦截所有响应，判断身份认证信息
+    options.complete = function(res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+$(function() {
+    // 获取用户基本信息
+    getUserInfo();
+    // 退出
+    $('#btnLogout').on('click', function() {
+        layer.confirm('是否确认退出登录?', { icon: 3, title: '提示' }, function(index) {
+            localStorage.removeItem('tooken');
+            location.href = '/login.html';
+            layer.close(index);
+        })
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message);
+            }
+            // 渲染头像和用户名
+            renderAvatar(res.data);
+        }
+    })
+}
+
+// 渲染头像和用户名
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    if (user.user_pic !== null) {
+        // 图片头像
+        $('.layui-nav-img').attr('url', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 文本头像
+        $('.layui-nav-img').hide();
+        var frist = name[0].toUpperCase();
+        $('.text-avatar').html(frist).show();
+    }
+}
